e516ba15ccea989c7c4a5bbaee4198158356bb0f
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2022-04-10 21:17:09 -0700

    added float check and exponent check

--- CalcMain.py
+++ CalcMain.py
@@ -11,9 +11,19 @@ def main():
 def input_valid(user_input):
     user_input = user_input.lower()
     input_length = len(user_input)
+    exp_place = user_input.find('e')
 
     if user_input[-1] == 'd' or user_input[-1] == 'f':
         user_input = user_input[:input_length-1]
+    else:
+        print("This is not a literal.")
+        return
+
+    if exp_place != -1:
+        string_split = user_input.split(exp_place)
+        number = string_split[0]
+        exponent = string_split[1]
+
     
     print(user_input)
 
