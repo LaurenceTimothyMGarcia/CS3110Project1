c07703140d6fe8304b830c2a9cb944fa730313af
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2022-04-10 20:55:13 -0700

    working on input validation

--- CalcMain.py
+++ CalcMain.py
@@ -3,3 +3,18 @@
 #Professor Dong
 #April 13th, 2022
 
+def main():
+    user_input = input("")
+
+    input_valid(user_input)
+
+def input_valid(user_input):
+    user_input = user_input.lower()
+    input_length = len(user_input)
+
+    if user_input[-1] == 'd' or user_input[-1] == 'f':
+        user_input = user_input[:input_length-1]
+    
+    print(user_input)
+
+main()
