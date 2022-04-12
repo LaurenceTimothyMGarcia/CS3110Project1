fdc2c21910fafeb108d9e49b9f42620918793e3f
EmergencyPlayer <45772910+LaurenceTimothyMGarcia@users.noreply.github.com>
2022-04-12 13:51:07 -0700

    Updated Dict and Had it rebuild code
    It rebuilds the input statement with for loops

--- CalcMain.py
+++ CalcMain.py
@@ -43,6 +43,7 @@ def convert_string(string):
     translate = json.load(num_dict)
 
     str_len = len(string)
+    print("String Length: ", str_len)
     str_pos = 0     #Position in String
     dec_loc = 0     #Position of Decimal
     exp_loc = 0     #Position of Exponent 'e'
@@ -52,26 +53,33 @@ def convert_string(string):
     for i in string:
         if i == '.':
             dec_loc = str_pos
-            print(dec_loc)
+            print("Decimal Position: ", dec_loc)
         if i == 'e':
             exp_loc = str_pos
-            print(exp_loc)
+            #print(exp_loc)
         str_pos += 1
 
+    str_pos = 0
     new_float = 0
     for i in string:
-        if str_pos == '.':
+        if i == '.':
+            str_pos += 1
             continue
 
-        if str_pos < dec_loc:
-            pow_10 = dec_loc - str_pos
-            print("Power of 10: " + pow_10)
+        #if str_pos < dec_loc:
+        print("String Position: ", str_pos)
+        if dec_loc < str_pos:
+            pow_10 =  dec_loc - str_pos
+        else:
+            pow_10 =  dec_loc - str_pos - 1
+
+        print("Power of: ", pow_10)
             
-            new_float += (translate[i] * (10**pow_10))
+        new_float += (translate[i] * (10**pow_10))
 
         str_pos += 1
-        print(translate[i])
-        print(new_float)
+        print("String Character: ", translate[i])
+        print("Float Number: ", new_float)
 
     num_dict.close()
 
