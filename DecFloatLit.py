2811a5b85fc82a5b71d207d4028dae6b2acce409
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2022-04-12 16:36:39 -0700

    removed negative

---
+++ DecFloatLit.py
@@ -0,0 +1,12 @@
+#Laurence Timothy M. Garcia
+#CS 3110
+#Professor Dong
+#April 13th, 2022
+
+import json
+
+def main():
+    user_input = input("Input String here (Type in exit(0) to quit): ")
+
+    while user_input != "exit(0)":
+        
