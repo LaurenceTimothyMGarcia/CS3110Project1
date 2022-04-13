a31f89be2bb8010057d2a35282055a152f3a247b
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2022-04-12 17:20:54 -0700

    Finished state 1

--- DecFloatLit.py
+++ DecFloatLit.py
@@ -13,7 +13,30 @@ def main():
         user_input = input("Input String here (Type in exit(0) to quit): ")
 
 def dfa_valid(user_input):
+    num_dict = open('NumAlpha.json')
+    translate = json.load(num_dict)
+
+    user_input = user_input.lower() #changes everything to lower case
+    input_len = len(user_input)     #provides length of string
+    char_pos = 0                     #Character position in string
+
+    for chr in user_input:
+        print("Char Pos:", char_pos)
+        print("Character is:", chr)
+
+        #State 1
+        #Checks if first input is either digit or decimal dot
+        if char_pos == 0:
+            if chr not in translate.keys() and chr != '.':
+                print("Not a valid input")
+                return
+        
+
+        
+        char_pos += 1
     
+    num_dict.close()
+    return
 
 
 #Running Code
