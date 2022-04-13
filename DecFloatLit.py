824b46921773182e6faac16a0fe6fe4845d4fd76
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2022-04-12 19:55:12 -0700

    can probably recreate the float

--- DecFloatLit.py
+++ DecFloatLit.py
@@ -18,9 +18,14 @@ def dfa_valid(user_input):
 
     user_input = user_input.lower() #changes everything to lower case
     input_len = len(user_input)     #provides length of string
+    dec_float_point = 0             #Float Point
+
     char_pos = 0                    #Character position in string
     dec_pos = user_input.find('.')  #Location of decimal dot
-    dec_check = False                     #Boolean for the decimal dot
+    dec_check = False               #Boolean for the decimal dot
+    exp_check = False               #Boolean for e
+
+    pow_10 = 0                      #Power of 10 to multiply by
 
     for chr in user_input:
         print("Char Pos:", char_pos)
@@ -42,12 +47,25 @@ def dfa_valid(user_input):
                 print("Not a valid input")
                 return
             else:
+                char_pos += 1
                 dec_check = True
+                continue
 
-        #if chr == 'e':
+        if dec_check and not exp_check:
+            pow_10 = dec_pos - char_pos
+        elif not dec_check and not exp_check:
+            pow_10 = dec_pos - char_pos - 1
+        
+        
 
+        dec_float_point += (translate[chr] * (10 ** pow_10))
         
         char_pos += 1
+
+        #Checks for testing
+        print("String character:", chr)
+        print("Dictionary equiv:", translate[chr])
+        print("Float Number:", dec_float_point)
     
     num_dict.close()
     return
